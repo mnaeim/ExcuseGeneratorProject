cf0c85e80a35cc3cacbaaa0371d6d106746bc04d
Mazen Naeim <naeimmazen@gmail.com>
2021-09-29 22:55:53 +0000

    few changes

--- src/app.js
+++ src/app.js
@@ -5,21 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function () {
-  console.log("hfhfyryrytytuty");
+window.onload = function() {
+  console.log("excuses");
 };
 function Excuse() {
-
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
-  let when = ["before the class", "right on time", "when I finished", "during my lunch", "while I was praying"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
   var who = [Math.floor(Math.random() * (who.length - 1))];
   var action = [Math.floor(Math.random() * (action.length - 1))];
   var what = [Math.floor(Math.random() * (what.length - 1))];
   var when = [Math.floor(Math.random() * (when.length - 1))];
 
-  document.getElementById('excuses').innerHTML = '<div>' who + ' ' + action + ' ' + what + ' ' + when. '</div>'
-
+  document.getElementById("excuses").innerHTML =
+    "<div>" + who + "" + action + "" + what + "" + when + "</div>";
 }
